050515d80c33c1acc04d9e6f10ab1a4f5b69aebd
hyh <test@test.com>
2017-12-06 22:41:04 +0800

    新增配置中心客户端

---
+++ cloud-hyh-config-client/src/main/java/com/hyh/ConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.hyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description :配置中心启动类
+ * @Author hanyahong
+ * @Date 2017/12/6- 14:06
+ */
+
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+
+}
+
---
+++ cloud-hyh-config-client/src/main/java/com/hyh/TestApi.java
@@ -0,0 +1,29 @@
+package com.hyh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description :配置中心-客户端展示API
+ * @Author hanyahong
+ * @Date 2017/12/6- 16:39
+ */
+
+@RestController
+@RefreshScope
+public class TestApi {
+
+    @Value("${myblog.name}")
+    private String name;
+    @Value("${myblog.url}")
+    private String url;
+    @Value("${myblog.location}")
+    private String location;
+
+    @RequestMapping("/blog-info")
+    public String getBlogInfo() {
+        return "从Github仓库中获取得到我博客信息：【"+location+","+","+url+","+name+"】";
+    }
+}
