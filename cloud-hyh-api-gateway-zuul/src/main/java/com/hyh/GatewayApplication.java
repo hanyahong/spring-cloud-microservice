efe856e575e0c105516706c82f05e11d9b78cf72
hanyahong <hyhvpn@gmail.com>
2017-12-17 19:01:35 +0800

    新增api网关服务

---
+++ cloud-hyh-api-gateway-zuul/src/main/java/com/hyh/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Administrator on 2017/12/17.
+ * 网关启动入口
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
